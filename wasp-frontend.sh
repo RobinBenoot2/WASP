431293a21f72dbb4eee4d4e3d7616271ac9dbbc6
RobinBenoot2 <robin.benoot@student.hogent.be>
2022-12-19 17:42:23 +0100

    changes for backend

---
+++ wasp-frontend.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Starting git clone from repository!"
+rm -r 2223-frontendweb-RobinBenoot2
+git clone https://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@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
+cd 2223-frontendweb-RobinBenoot2
+echo "Building the image!"
+docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-frontend:latest
+echo "Run container"
+docker stop wasp-frontend
+docker rm wasp-frontend
+docker run --restart unless-stopped -p 5173:5173 -d --name wasp-frontend 192.168.50.5:5000/wasp-frontend:latest 
