c80d6e2fcfabbb6f4d4268de51d3fb0f5b3831c5
RobinBenoot2 <robin.benoot@student.hogent.be>
2023-01-18 15:21:25 +0100

    new method for cloning repo's

--- wasp-backend.sh
+++ wasp-backend.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 echo "Starting git clone from repository!"
 rm -r 2223-webservices-RobinBenoot2
-git clone https://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@github.com/Web-IV/2223-webservices-RobinBenoot2.git
+# requires a script file in the github folder called authentication.sh
+# with the following content:
+# export GITHUB_USER="yourusername"
+# export GitHub_TOKEN="yourtoken"
+bash /mnt/user/WASP/github/authentication.sh
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/Web-IV/2223-webservices-RobinBenoot2.git
 cd 2223-webservices-RobinBenoot2
 echo "Building the image!"
 docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-backend:latest
--- wasp-frontend.sh
+++ wasp-frontend.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Starting git clone from repository!"
 rm -r 2223-frontendweb-RobinBenoot2
-git clone https://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@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
+bash /mnt/user/WASP/github/authentication.sh
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
 cd 2223-frontendweb-RobinBenoot2
 echo "Building the image!"
 docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-frontend:latest
