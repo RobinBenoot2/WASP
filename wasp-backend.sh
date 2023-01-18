b4c8f727961bb4eaab214e76595f2e594bed1b70
RobinBenoot2 <robin.benoot@student.hogent.be>
2023-01-18 01:04:36 +0100

    update to latest changes

--- wasp-backend.sh
+++ wasp-backend.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Starting git clone from repository!"
 rm -r 2223-webservices-RobinBenoot2
-git clone https://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@github.com/Web-IV/2223-webservices-RobinBenoot2.git
+git clone https://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@github.com/Web-IV/2223-webservices-RobinBenoot2.git
 cd 2223-webservices-RobinBenoot2
 echo "Building the image!"
 docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-backend:latest
 echo "Run container"
 docker stop wasp-backend
 docker rm wasp-backend
-docker run --restart unless-stopped -p 7000:7000 -d --name wasp-backend 192.168.50.5:5000/wasp-backend:latest 
+docker run --restart unless-stopped -p 9500:9500 -d --name wasp-backend 192.168.50.5:5000/wasp-backend:latest 
--- wasp-frontend.sh
+++ wasp-frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting git clone from repository!"
 rm -r 2223-frontendweb-RobinBenoot2
-git clone https://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@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
+git clone https://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@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
 cd 2223-frontendweb-RobinBenoot2
 echo "Building the image!"
 docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-frontend:latest
