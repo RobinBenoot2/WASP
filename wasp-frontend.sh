6813c0ea5046c51bb2c7aa9094d685fbfb3eb57b
RobinBenoot2 <100361474+RobinBenoot2@users.noreply.github.com>
2023-02-14 19:06:33 +0100

    Add files via upload

--- wasp-frontend.sh
+++ wasp-frontend.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 echo "Starting git clone from repository!"
-sh /home/scripts/authentication.sh
-if [ -d "/home/scripts/2223-frontendweb-RobinBenoot2" ]; then
+if [ -d "/home/repo/wasp-frontend/2223-frontendweb-RobinBenoot2" ]; then
   echo "Directory exists."
   cd 2223-frontendweb-RobinBenoot2
   git pull
-  cd ..
 else
   echo "Directory does not exist."
+  cd /home/repo/wasp-frontend
   git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/Web-IV/2223-frontendweb-RobinBenoot2.git
+  cd 2223-frontendweb-RobinBenoot2
 fi
-cd 2223-frontendweb-RobinBenoot2
 echo "Building the image!"
 docker build . --file Dockerfile --tag 192.168.50.5:5000/wasp-frontend:latest
 echo "Run container"
